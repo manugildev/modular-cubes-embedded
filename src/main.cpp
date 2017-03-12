d1449997832156445da4734fbfc2a124048a7680
ManuGil <madtriangl3@gmail.com>
2017-03-12 23:56:28 +0100

    Cleanup
    - Firebase Library Removed
    - WiFiClient code removed (From now on I will use ESP8266HTTPClient)

--- src/main.cpp
+++ src/main.cpp
@@ -33,53 +33,7 @@ WiFiClient client;
 void loop() {
   delay(5000);
 
-  Serial.print("connecting to ");
-  Serial.println(host);
-
-  // Use WiFiClient class to create TCP connections
-  const int httpPort = 80;
-  if (!client.connect(host, httpPort)) {
-    Serial.println("connection failed");
-    return;
-  }
-
-  // We now create a URI for the request
-  String url = "/comments/";
-  url += "1";
-
-  Serial.print("Requesting URL: ");
-  Serial.println(url);
-
-  // This will send the request to the server
-  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-               "Host: " + host + "\r\n" +
-               "Connection: close\r\n\r\n");
-  unsigned long timeout = millis();
-  while (client.available() == 0) {
-    if (millis() - timeout > 5000) {
-      Serial.println(">>> Client Timeout !");
-      client.stop();
-      return;
-    }
-  }
-
-  // Read all the lines of the reply from server and print them to Serial
-  String json = "";
-  boolean httpBody = false;
-  while (client.available()) {
-    String line = client.readStringUntil('\r');
-    if (!httpBody && line.charAt(1) == '{') {
-      httpBody = true;
-    }
-    if (httpBody) {
-      json += line;
-    }
-  }
-
-  Serial.println(json);
-  Serial.println("closing connection");
-
-  //Testing HTTPClient
+  // HTTPClient
   HTTPClient http;
   http.begin("http://jsonplaceholder.typicode.com/users/1");
   http.addHeader("Content-Type", "text/plain");
