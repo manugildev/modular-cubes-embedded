c7b69edf72e95722744e7e1104aa39d0825e063a
ManuGil <madtriangl3@gmail.com>
2017-03-13 01:46:59 +0100

    Firebase GET

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,11 @@
 #include <Wire.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
-#include <UniversalTelegramBot.h>
+#include <WiFiClientSecure.h>
 
 // Custom
 #include <Server.h>
 #include <Configuration.h>
-
-#define BotToken "XXXXXXXXXXXXXXXXXXXXX"
-WiFiClientSecure secured_client;
-UniversalTelegramBot bot(BotToken, secured_client);
 
 void setup() {
   Serial.begin(9600);
@@ -31,16 +27,38 @@ void setup() {
 }
 
 void loop() {
+  WiFiClientSecure client;
+
+  String url = "/numbers.json";
+  String data = "";
+  Serial.print("GET to https://");
+  Serial.print(configuration.firebase_host);
+  Serial.println(url);
+
+  Serial.print("Result(response): ");
+  if (client.connect(configuration.firebase_host, 443)) {
+
+    client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+              "Host: " + configuration.firebase_host + "\r\n" +
+              "User-Agent: ESP8266\r\n" +
+              "Connection: close\r\n\r\n");
+
+    delay(10);
+
+    while(client.available()){
+       char c = client.read();
+       Serial.print(c);
+    }
+  } else {
+    Serial.println("ERROR");
+  }
   delay(5000);
-
-  bot.sendMessage("chat_id", "Test Message from ESP8266", "Markdown");
-
   // HTTPClient
   HTTPClient http;
   http.begin("http://jsonplaceholder.typicode.com/users/1");
   http.addHeader("Content-Type", "text/plain");
   int httpCode = http.GET();
-
+  Serial.println();
   Serial.print("HTTP response code: ");
   Serial.println(httpCode);
 
