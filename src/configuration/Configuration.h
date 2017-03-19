bc704a0dd262cef08263c55dfbf70a411d24e915
ManuGil <madtriangl3@gmail.com>
2017-03-19 17:42:12 +0100

    Configuration Updated

--- src/configuration/Configuration.h
+++ src/configuration/Configuration.h
@@ -1,9 +1,10 @@
-#define WIFI_SSID "XXXXXXXXX"
-#define WIFI_PASSWORD "XXXXXXXXX"
-#define CUBES_WIFI_SSID "XXXXXXXXX"
-#define CUBES_WIFI_PASSWORD "XXXXXXXXX"
-#define FIREBASE_HOST "XXXXXXXXX"
-#define FIREBASE_AUTH "XXXXXXXXX"
+#define WIFI_SSID "XXXXXXXX"
+#define WIFI_PASSWORD "XXXXXXXX"
+#define CUBES_WIFI_SSID "XXXXXXXX"
+#define CUBES_WIFI_PASSWORD "XXXXXXXX"
+#define FIREBASE_HOST "XXXXXXXX"
+#define FIREBASE_AUTH "XXXXXXXX"
+#define APPLE_MIDI_SESSION "XXXXXXXX"
 
 #define DEBUG true
 
@@ -15,5 +16,6 @@ public:
   const char *cubes_pass = CUBES_WIFI_PASSWORD;
   const char *firebase_host = FIREBASE_HOST;
   const char *firebase_auth = FIREBASE_AUTH;
+  const char *apple_midi_session = APPLE_MIDI_SESSION;
 };
 extern Configuration configuration;
